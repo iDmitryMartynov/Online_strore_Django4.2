15ba28607f7aae34114e239932824ef4efdc1f3e
Dmitry <dmitry1298@yandex.ru>
2024-11-11 14:33:36 +1000

    Set static and complete index.html

--- main/views.py
+++ main/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Index page')
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина "Home"',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': False
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
